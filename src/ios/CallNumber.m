eb3a002d9bb20d440359ccc00c5ed268a400ba6f
Nishil Shah <nishil.shah17@gmail.com>
2014-07-15 17:00:12 -0400

    iOS call number

--- src/ios/CallNumber.m
+++ src/ios/CallNumber.m
@@ -1,1 +1,10 @@
 #import <Cordova/CDVPlugin.h>
+#import "CallNumber.h"
+
+- (void) callNumber:(CDVInvokedUrlCommand*)command {
+
+  NSNumber* number = [command.arguments objectAtIndex:0];
+  NSString *phoneNumber = [@"telprompt://" stringByAppendingString:number];
+  [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phoneNumber]];
+
+}

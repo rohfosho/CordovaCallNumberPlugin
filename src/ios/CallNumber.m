632b9f6de576cb8c589d6812e565a7a47324e70e
Nishil Shah <nishil.shah17@gmail.com>
2014-07-17 09:48:02 -0400

    implementation clamber

--- src/ios/CallNumber.m
+++ src/ios/CallNumber.m
@@ -1,10 +1,14 @@
 #import <Cordova/CDVPlugin.h>
 #import "CallNumber.h"
 
+@implementation CallNumber
+
 - (void) callNumber:(CDVInvokedUrlCommand*)command {
 
-  NSNumber* number = [command.arguments objectAtIndex:0];
+  NSString* number = [command.arguments objectAtIndex:0];
   NSString *phoneNumber = [@"telprompt://" stringByAppendingString:number];
   [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phoneNumber]];
 
 }
+
+@end

3121048e0756e4812cf9aec9271571baee976197
Carlos Ferreyra <ferreyr@gmail.com>
2015-02-19 16:59:17 -0600

    updated plugin for ios

--- src/ios/CallNumber.m
+++ src/ios/CallNumber.m
@@ -5,9 +5,23 @@
 
 - (void) callNumber:(CDVInvokedUrlCommand*)command {
 
-  NSString* number = [command.arguments objectAtIndex:0];
-  NSString *phoneNumber = [@"telprompt://" stringByAppendingString:number];
-  [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phoneNumber]];
+ 	NSString* number = [command.arguments objectAtIndex:0];
+  	number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]
+
+ 	if( ! [string hasPrefix:@"tel:"]){
+		number =  [NSString stringWithFormat:@"tel:%@", number];
+ 	}	
+
+ 	if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]]) {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
+	}
+	else {
+		// missing phone number
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
+	}
+
+	// return result
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 
 }
 

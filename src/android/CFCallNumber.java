f50c3e18848afedc338e415a105d6f93002cdc5a
Carlos Ferreyra <ferreyr@gmail.com>
2015-02-20 15:59:39 -0600

    fxing package and sintax errors

--- src/android/CFCallNumber.java
+++ src/android/CFCallNumber.java
@@ -1,29 +1,31 @@
-package com.monmouth.callnumber;
+package mx.ferreyra.callnumber;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.util.Log;
+import android.telephony.TelephonyManager;
 
 public class CFCallNumber extends CordovaPlugin
 {
-	@Override
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
+
+        String number = args.getString(0);
+
         if( number.startsWith("tel:") == false){
             number = String.format("tel:%s", number);
         }
 
 
-        if (((TelephonyManager)cordova.getActivity().getContext().getSystemService(Context.TELEPHONY_SERVICE)).getPhoneType() == TelephonyManager.PHONE_TYPE_NONE ){
+        if (((TelephonyManager)cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE)).getPhoneType() == TelephonyManager.PHONE_TYPE_NONE ){
             callbackContext.error("NoFeatureCallSupported");
         }
-        
-        {
+
         try {
             Intent intent = new Intent(Intent.ACTION_CALL);
             intent.setData(Uri.parse(number));

db7a18ad5d4aed0c62bc7372607a9a5cc2d2cfa3
Nishil Shah <nishil.shah17@gmail.com>
2015-06-19 12:55:43 -0400

    bug fix

--- src/ios/CFCallNumber.m
+++ src/ios/CFCallNumber.m
@@ -4,28 +4,30 @@
 @implementation CFCallNumber
 
 - (void) callNumber:(CDVInvokedUrlCommand*)command {
-    
+
     [self.commandDelegate runInBackground:^{
-        
+
         CDVPluginResult* pluginResult = nil;
         NSString* number = [command.arguments objectAtIndex:0];
         number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        
+
         if( ! [number hasPrefix:@"tel:"]){
             number =  [NSString stringWithFormat:@"tel:%@", number];
         }
-        
+
         if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:number]]) {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
         }
-        else {
+        else if ([number length] == 0){
             // missing phone number
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
+        } else {
+          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         }
-        
+
         // return result
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        
+
     }];
 }
 

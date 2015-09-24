3dad50b5dfc292f0a20088b6d5a5dfa9b0b795ee
Nishil Shah <nishil.shah17@gmail.com>
2015-09-24 16:58:51 -0500

    oops

--- src/android/CFCallNumber.java
+++ src/android/CFCallNumber.java
@@ -15,8 +15,8 @@ public class CFCallNumber extends CordovaPlugin
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         String number = args.getString(0);
-        number.replaceAll("#","%23");
-
+        number = number.replaceAll("#","%23");
+        
         if( number.startsWith("tel:") == false){
             number = String.format("tel:%s", number);
         }

20fa5066e09ff4766642918b46af7836481bc03f
Juan Francisco Catalina Quintana <juanfrancatalina@hotmail.com>
2017-03-30 11:52:13 +0200

    Update CFCallNumber.m
    Solved problem in iOS that prevented the app from making phone calls

--- src/ios/CFCallNumber.m
+++ src/ios/CFCallNumber.m
@@ -1,34 +1,24 @@
 #import <Cordova/CDVPlugin.h>
 #import "CFCallNumber.h"
-
+ 
 @implementation CFCallNumber
-
+ 
 - (void) callNumber:(CDVInvokedUrlCommand*)command {
-
+ 
     [self.commandDelegate runInBackground:^{
-
+ 
         CDVPluginResult* pluginResult = nil;
         NSString* number = [command.arguments objectAtIndex:0];
-        number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-
-        if( ! [number hasPrefix:@"tel:"]){
-            number =  [NSString stringWithFormat:@"tel:%@", number];
-        }
-
-        if(![[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:number]]) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
-        }
-        else if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:number]]) {
-            // missing phone number
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
-        } else {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        }
-
+ 
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+ 
         // return result
+        NSString *varyingString1 = @"tel:";
+        NSString *s = [NSString stringWithFormat: @"%@%@", varyingString1, number];
+ 
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:s]];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
     }];
 }
-
+ 
 @end
